272058100b2dd40421d7e656135ce9af0942d844
Reinaldo Romero <reinaldoromero696@gmail.com>
2024-08-08 11:39:57 -0400

    add mobile responsive change

--- includes/header.php
+++ includes/header.php
@@ -16,15 +16,11 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,600;0,700;1,500&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css" integrity="sha512-17EgCFERpgZKcm0j0fEq1YCJuyAWdz9KUtv1EjVuaOz8pDnh/0nZxmU6BBXwaaxqoi9PQXnRWqlcDB027hgv9A==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.css" integrity="sha512-wR4oNhLBHf7smjy0K4oqzdWumd+r5/+6QO/vDda76MW5iug4PT7v86FoEkySIJft3XA0Ae6axhIvHrqwm793Nw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
-<<<<<<< Updated upstream
-=======
     <link rel="stylesheet" href="assets/css/responsive-gsap.css">
->>>>>>> Stashed changes
     <link rel="stylesheet" href="assets/fonts/styles.css">
     <link rel="stylesheet" href="assets/css/style.css">
     <link rel="stylesheet" href="assets/css/style-gsap.css">
     <link rel="stylesheet" href="assets/css/responsive.css">
-    
     <!-- End CSS Files Includes !-->
 </head>
 
